771a5cf677eff3fc857072b5386d4f19934fa0a4
ksmaze <kafkasmaze@gmail.com>
2026-01-19 18:23:30 +0000

    Cleanup

--- packages/app/src-tauri/build.rs
+++ packages/app/src-tauri/build.rs
@@ -1,10 +1,3 @@
 fn main() {
-    let target = std::env::var("TARGET").unwrap_or_default();
-    if target.contains("android") {
-        if std::env::var_os("TAURI_CONFIG").is_none() {
-            std::env::set_var("TAURI_CONFIG", r#"{"bundle":{"externalBin":[]}}"#);
-        }
-    }
-
     tauri_build::build()
 }
